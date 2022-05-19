5791e21ab5c4b5d654d3d3335065ab498b38a5fa
Vitaliy Bunin <v.p.bunin@gmail.com>
2022-05-19 21:26:36 +0300

    a

---
+++ software/arduino/sketch_may19a/defines.h
@@ -0,0 +1,51 @@
+#define RDY_PORT    PORTC
+#define RDY_DDR     DDRC
+#define RDY_PIN     PIN6
+
+#define WR_PORT     PORTC
+#define WR_DDR      DDRC
+#define WR_PIN      PIN7
+
+#define D7_PORT     PORTA
+#define D7_DDR      DDRA
+#define D7_PIN      PIN7
+
+#define D6_PORT     PORTA
+#define D6_DDR      DDRA
+#define D6_PIN      PIN6
+
+#define D5_PORT     PORTA
+#define D5_DDR      DDRA
+#define D5_PIN      PIN5
+
+#define D4_PORT     PORTA
+#define D4_DDR      DDRA
+#define D4_PIN      PIN4
+
+#define D3_PORT     PORTA
+#define D3_DDR      DDRA
+#define D3_PIN      PIN3
+
+#define D2_PORT     PORTA
+#define D2_DDR      DDRA
+#define D2_PIN      PIN2
+
+#define D1_PORT     PORTA
+#define D1_DDR      DDRA
+#define D1_PIN      PIN1
+
+#define D0_PORT     PORTA
+#define D0_DDR      DDRA
+#define D0_PIN      PIN0
+
+#define ENC_A_PORT  PORTC
+#define ENC_A_DDR   DDRC
+#define ENC_A_PIN   PIN0
+
+#define ENC_B_PORT  PORTC
+#define ENC_B_DDR   DDRC
+#define ENC_B_PIN   PIN1
+
+#define ENC_BTN_PORT PORTC
+#define ENC_BTN_DDR  DDRC
+#define ENC_BTN_PIN  PIN2
